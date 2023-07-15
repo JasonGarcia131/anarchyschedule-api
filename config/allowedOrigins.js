1a7a0d5d9a3725ecc00f1364cc95beef610032da
Jason Garcia <jaygarcia0530@gmail.com>
2023-07-15 16:07:55 -0700

    updated allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    'http://www.anarchy-schedule.onrender.com',
-    'https://www.anarchy-schedule.onrender.com',
+    'http://anarchy-schedule.onrender.com',
+    'https://anarchy-schedule.onrender.com',
 ];
 
 module.exports = allowedOrigins;
